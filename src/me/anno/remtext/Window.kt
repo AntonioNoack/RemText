4f4504c25c692a6dca50ab8e198f13ad04601917
Antonio Noack <antonio-noack@gmx.de>
2025-10-20 23:53:33 +0200

    Tried setting icon...

--- src/me/anno/remtext/Window.kt
+++ src/me/anno/remtext/Window.kt
@@ -3,9 +3,13 @@ package me.anno.remtext
 import me.anno.remtext.Rendering.file
 import me.anno.remtext.Rendering.gfxCheck
 import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWImage
 import org.lwjgl.opengl.GL
 import java.awt.MouseInfo
 import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import javax.imageio.ImageIO
 
 object Window {
 
@@ -29,6 +33,11 @@ object Window {
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
 
+        // for this to work, there must be a RemText.desktop
+        val appName = "RemText"
+        glfwWindowHintString(GLFW_X11_CLASS_NAME, appName)
+        glfwWindowHintString(GLFW_X11_INSTANCE_NAME, appName)
+
         val width = 800
         val height = 600
         window = glfwCreateWindow(width, height, "LWJGL Quad Example", NULL, NULL)
@@ -39,16 +48,27 @@ object Window {
         val windowX = mouse0.x - width.shr(1)
         val windowY = mouse0.y - height.shr(1)
         glfwSetWindowPos(window, windowX, windowY)
-
         glfwSetWindowTitle(window, "$WINDOW_TITLE - ${file.file.name}")
+        if (false) setIcon()
 
-        // todo get this to work...
-        /*val icon = ImageIO.read(Window.javaClass.getResourceAsStream("/Icon256.png"))
+        glfwMakeContextCurrent(window)
+        glfwSwapInterval(1) // vsync
+        glfwShowWindow(window)
+        GL.createCapabilities()
+
+        gfxCheck()
+    }
+
+    private fun setIcon() {
+        // todo get this to work... doesn't show up at all on Ubuntu :(
+        val icon = ImageIO.read(Window.javaClass.getResourceAsStream("/Icon256.png"))
         val image = GLFWImage.malloc()
-        val pixelBytes = ByteBuffer.allocateDirect(icon.width * icon.height * 4).order(ByteOrder.native())
-        for(y in 0 until icon.height) {
-            for(x in 0 until icon.width) {
-                val rgb = icon.getRGB(x,y)
+        val pixelBytes = ByteBuffer
+            .allocateDirect(icon.width * icon.height * 4)
+            .order(ByteOrder.nativeOrder())
+        for (y in 0 until icon.height) {
+            for (x in 0 until icon.width) {
+                val rgb = icon.getRGB(x, y)
                 // channels are not tested yet
                 pixelBytes.put((rgb shr 24).toByte())
                 pixelBytes.put((rgb shr 16).toByte())
@@ -62,14 +82,6 @@ object Window {
         val buffer = GLFWImage.malloc(1)
         buffer.put(0, image)
         glfwSetWindowIcon(window, buffer)
-        buffer.free()*/
-
-        glfwMakeContextCurrent(window)
-        glfwSwapInterval(1) // vsync
-        glfwShowWindow(window)
-        GL.createCapabilities()
-
-        gfxCheck()
+        buffer.free()
     }
-
 }
